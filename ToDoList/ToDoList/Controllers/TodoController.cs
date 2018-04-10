e26e9f850286608cff19d9492b21141479b2a929
Joshua Taylor <taylor.joshua88@gmail.com>
2018-04-10 11:55:41 -0700

    Set up initial GET routes and established DbContext

---
+++ ToDoList/ToDoList/Controllers/TodoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+
+namespace ToDoList.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoDbContext _context;
+
+        public TodoController(TodoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.TodoItems.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int? id)
+        {
+            try
+            {
+                return Ok(await _context.TodoItems.SingleAsync(i => i.Id == id));
+            }
+            catch (Exception)
+            {
+                // TODO: Insert logging here
+                return NotFound();
+            }
+        }
+    }
+}
